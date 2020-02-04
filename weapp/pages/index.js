7e9e5f5e7a5c6463ececa9ccc227a710220fb8ba
liangacg <liangacg@126.com>
2020-02-04 10:26:45 +0800

    Add files via upload

---
+++ weapp/pages/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var _core = _interopRequireDefault(require('./../vendor.js')(0));
+
+var _store = _interopRequireDefault(require('./../store/index.js'));
+
+var _x = require('./../vendor.js')(4);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+_core["default"].page({
+  data: {}
+}, {info: {"components":{},"on":{}}, handlers: {}, models: {}, refs: undefined });
